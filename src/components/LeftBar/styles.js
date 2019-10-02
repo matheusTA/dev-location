3ebbc5a83f380e377ea032330276175ce8ff3a7f
MatheusTA <matrajano12@gmail.com>
2019-10-02 03:42:53 -0300

    Criando e estilizando leftBar

---
+++ src/components/LeftBar/styles.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #F5F5F5;
+  width: 20%;
+  height: 90%;
+  position: absolute;
+  top: 5%;
+  left: 2%;
+  border-radius: 5px;
+  overflow: auto;
+`;
+
+export const ListUsers = styled.ul`
+  list-style: none;
+  padding: 5%;
+`;
+
+export const User = styled.li`
+  display: flex;
+  flex-direction: row;
+  padding: 5%;
+  align-items: center;
+  border-bottom: 1px solid rgba(190, 190, 190, 0.6 );
+
+  img {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    margin-right: 2%;
+  }
+`;
+
+export const UserInfo = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  p {
+    color: #666;
+    font-weight: 300;
+  }
+
+  button {
+    background: transparent;
+    color: #E90C07;
+    margin-right: 10px;
+  }
+`;
