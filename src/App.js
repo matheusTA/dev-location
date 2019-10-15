56c973b35e65ea12e6bbe50c0ceec929517189c9
MatheusTA <matrajano12@gmail.com>
2019-10-15 02:19:42 -0300

    Adicionando provider com store

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import './config/ReactotronConfig';
+import { Provider } from 'react-redux';
 import Routes from './routes';
+import store from './store';
 import GlobalStyles from './styles/globalStyles';
 
-function App() {
-  return (
-    <>
-      <Routes />
-      <GlobalStyles />
-    </>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <Routes />
+    <GlobalStyles />
+  </Provider>
+
+);
 
 export default App;
